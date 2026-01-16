f0d62872d4ce9d8b2c2a694247a7936b55abfc0b
Michael Zhao <mchlzhao@gmail.com>
2026-01-16 11:40:44 -0500

    started basic project

---
+++ vocab-builder/vocab-builder/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  vocab-builder
+//
+//  Created by Michael Zhao on 1/16/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
